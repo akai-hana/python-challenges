75e1b6d4d74c32b6ee36f95cc68459240d2cff31
Matias Moya <55887501+akai-hana@users.noreply.github.com>
2024-01-31 08:49:00 +0100

    Add 03 - Usuaris autoritzats (Exemple)

---
+++ Replit/03 Bucles. Llistes. Estructures repetitives./03 Usuaris autoritzats (Exemple).py
@@ -0,0 +1,10 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+elif username == "aland":
+  print("Hey there Aland!")
+else:
+  print("Go away!")
